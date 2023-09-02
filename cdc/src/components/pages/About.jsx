ddf2eddce3efc82ce0c6e83758ff7e063013463b
JayashreeTJ2004 <shreejai4002@gmail.com>
2023-09-02 23:59:54 +0530

    commit

--- cdc/src/components/pages/About.jsx
+++ cdc/src/components/pages/About.jsx
@@ -4,30 +4,31 @@ import "../Tab.css";
 export const About = ()=>{
     return (
         <div className="container">
-
-      <h3 style={{}}>ABOUT US</h3>
-      <div style={{display:"flex",flexDirection:"row",justifyContent:"space-evenly"}}>
-          <div style={{width:"50%"}}>
+ 
+      <h3 style={{marginTop:"3%"}}>ABOUT US</h3>
+      <div className="space">
+         
           <p style={{lineHeight: '2'}}>
 Coimbatore Dermal Centre is a leading dermatology 
 establishment in Gopalapuram, Coimbatore. With a strong 
 focus on customer satisfaction, they offer a wide range of services,
  including skin care clinics, cosmetic surgery, and aesthetic dermatology.
   The dedicated team strives to achieve the company's common vision and goals.
-   Located near Citi Tower, Gandhipuram, commuting to the center is easy.
+   Located near Citi Tower, Gandhipuram, commuti ng to the center is easy.
     Their expertise in addressing various skin concerns and beauty-related
      issues sets them apart as a top player in the field. Trusted by a vast 
      
      customer base, they continue to expand their offerings to cater to more
       clients.
       </p>
-          </div>
+          
          
-      <div style={{width:"50%",marginLeft:'20px'}}>
-      <img src="H.jpg" style={{Width: "100%", height: "auto"}}/>
+    
+      <img src="H.jpg" style={{Width: "100%",height:"auto",marginLeft:'20px'}}/>
+      
       </div>
      
-      </div>
+      
         
             </div>
     )

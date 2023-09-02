007b9c05997002b0fcb0cb85bcc1fbd6df4c9662
Pragadeeswari277 <pragadeeswari.g2004@gmail.com>
2023-09-02 21:45:15 +0530

    commit

--- cdc/src/components/pages/Home.jsx
+++ cdc/src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 export const Home = ()=>{
-    return <div>
+    return(
+        <div>
         Home
-        </div>;
-};
+       
+        </div>
+    )
+        
+    
+}
